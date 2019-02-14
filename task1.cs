3f686338731706a0ecaddab4f4fe53423b9164c0
Kazyna <45530319+Issakul@users.noreply.github.com>
2019-02-14 08:08:30 -0800

    week 1
    lab1

---
+++ task1.cs
@@ -0,0 +1,3 @@
+﻿1>------ Build started: Project: ConsoleApp2, Configuration: Debug Any CPU ------
+1>C:\Users\Issakul\source\repos\ConsoleApp2\ConsoleApp2\Program.cs(20,30,20,31): error CS0136: A local or parameter named 'i' cannot be declared in this scope because that name is used in an enclosing local scope to define a local or parameter
+========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========
