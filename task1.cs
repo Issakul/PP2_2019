e13a2aa8a9094496e232e9e4966dc7fcbf2e4bc7
Kazyna <45530319+Issakul@users.noreply.github.com>
2019-02-14 08:11:10 -0800

    week 2
    lab 2

--- task1.cs
+++ task1.cs
@@ -1,3 +1,33 @@
-﻿1>------ Build started: Project: ConsoleApp2, Configuration: Debug Any CPU ------
-1>C:\Users\Issakul\source\repos\ConsoleApp2\ConsoleApp2\Program.cs(20,30,20,31): error CS0136: A local or parameter named 'i' cannot be declared in this scope because that name is used in an enclosing local scope to define a local or parameter
-========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool isPalindrome(string s)//метод для определения является ли строка палиндромом
+        {
+            if (s.Length == 1)//если вводимое слово это буква
+                return true;//значит это палиндром
+            else if (s[0] == s[s.Length - 1])//в другом случае, если первый символ строки равен последнему
+            {
+                return isPalindrome(s.Substring(1, s.Length - 2));//то мы рекурсивно возвращаем метод от строки укороченной с начала и с конца на один символ
+            }
+            return false;//если строка не прошла проверку то возвращаем false
+        }
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");//создаем переменную для чтения данных с файла input.txt
+            string sl = sr.ReadLine();//читаем первую строку
+            if (isPalindrome(sl) == true)//если метод от прочтенной строки возвращает true
+                Console.WriteLine("Yes");//то выводим на консоль Yes
+            else//в обратном случае
+                Console.WriteLine("No");//вывод No
+            Console.ReadKey();//консоль закрывается при нажатии клавиши
+        }
+        }
+        }
